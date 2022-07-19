7092edb1247cbc172528c851fffcc4071c8c3ca7
Calvin Wilkinson <kinsondigital@gmail.com>
2022-07-19 12:42:05 +0100

    Create service to set GitHub action output values

---
+++ KDActionUtils/Services/ActionOutputService.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="ActionOutputService.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using KDActionUtils.Exceptions;
+
+namespace KDActionUtils.Services;
+
+/// <inheritdoc/>
+public class ActionOutputService : IActionOutputService
+{
+    private readonly IConsoleService<ConsoleContext> consoleService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ActionOutputService"/> class.
+    /// </summary>
+    /// <param name="consoleService">Writes to the console.</param>
+    public ActionOutputService(IConsoleService<ConsoleContext> consoleService) => this.consoleService = consoleService;
+
+    /// <inheritdoc/>
+    public void SetOutputValue(string name, string value)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new NullOrEmptyStringArgumentException($"The parameter '{nameof(name)}' must not be null or empty.");
+        }
+
+        this.consoleService.WriteLine($"::set-output name={name}::{value}");
+    }
+}
