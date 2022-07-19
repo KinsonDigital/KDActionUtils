239b54f181f4368fbbb199d8ef84790050a90ee9
Calvin Wilkinson <kinsondigital@gmail.com>
2022-07-19 12:10:03 +0100

    Create a GitHub type of console service

---
+++ KDActionUtils/Services/GitHubConsoleService.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="GitHubConsoleService.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace KDActionUtils.Services;
+
+/// <summary>
+/// Provides functionality for writing to a GitHub action console.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class GitHubConsoleService : ConsoleService
+{
+    /// <inheritdoc/>
+    public override ConsoleContext ConsoleContext { get; set; } = ConsoleContext.GitHubAction;
+
+    /// <inheritdoc/>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     Thrown if the <see cref="IConsoleService{T}.ConsoleContext"/> is an invalid value.
+    /// </exception>
+    public override void StartGroup(string name)
+        => Console.WriteLine($"::group::{(string.IsNullOrEmpty(name) ? "Group" : name)}");
+
+    /// <inheritdoc/>
+    public override void EndGroup() => Console.WriteLine("::endgroup::");
+
+    /// <inheritdoc/>
+    public override void WriteError(string value) => Console.WriteLine($"::error::{value}");
+}
